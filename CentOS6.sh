b87bb045d82bae045de7f952d542cbefc3e0007f
2663076774 <67045869+2663076774@users.noreply.github.com>
2022-02-10 15:03:17 +0800

    Update CentOS6.sh

--- CentOS6.sh
+++ CentOS6.sh
@@ -1,2 +1,3 @@
-rm -rf /etc/yum.repos.d/CentOS* && wget --no-check-certificate -O /etc/yum.repos.d/CentOS-Base.repo https://cdn.jsdelivr.net/gh/Xiaotiantian001/yumrepo/CentOS6-Base.repo
-rm -rf /home/Centos6.5_64.tar.gz && wget --no-check-certificate -O /home/Centos6.5_64.tar.gz https://cdn.jsdelivr.net/gh/Xiaotiantian001/yumrepo/Centos6.5_64.tar.gz
+rm -rf /etc/yum.repos.d/CentOS* && wget --no-check-certificate -O /etc/yum.repos.d/CentOS-Base.repo https://raw.fastgit.org/2663076774/yumrepo/CentOS6-Base.repo
+rm -rf /etc/yum.repos.d/epel.repo && wget --no-check-certificate -O /etc/yum.repos.d/epel.repo https://raw.fastgit.org/2663076774/yumrepo/main/epel.repo
+rm -rf /home/Centos6.5_64.tar.gz && wget --no-check-certificate -O /home/Centos6.5_64.tar.gz https://cdn.jsdelivr.net/gh/2663076774/yumrepo/Centos6.5_64.tar.gz
